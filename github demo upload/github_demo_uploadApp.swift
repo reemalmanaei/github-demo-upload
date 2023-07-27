37c909bda2f394ae3da917031ec4bd39bb36ba66
Reem Al Manaei <s658556@ese.gov.ae>
2023-07-27 11:23:41 +0400

    Initial Commit

---
+++ github demo upload/github_demo_uploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_demo_uploadApp.swift
+//  github demo upload
+//
+//  Created by Reem . on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct github_demo_uploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
