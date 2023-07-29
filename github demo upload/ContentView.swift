d1a5d0ae672c3647df8337071e590e310246be5f
Reem Al Manaei <s658556@ese.gov.ae>
2023-07-30 00:24:37 +0400

    made some visual changes to our text

--- github demo upload/ContentView.swift
+++ github demo upload/ContentView.swift
@@ -14,7 +14,11 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Text("Hey there")
         }
+        .multilineTextAlignment(.center)
+        .font(.system(size: 100))
+        .foregroundColor(.blue)
         .padding()
     }
 }
